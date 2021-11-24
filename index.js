f7d3cf01da5f8a19c8a9c2f3a8c1a2049d7ad1c0
Lioncat2002 <sankhayan2002@gmail.com>
2021-11-24 22:40:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
 } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+      alert("Geolocation is not supported by this browser.");
 }
 
 function showPosition(position){
